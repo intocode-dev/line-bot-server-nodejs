68470a1d34d957b0ca2cd7748d65c0b45664a706
Mick Charuwichitratana <micksatana@live.com>
2017-11-28 18:15:30 +0700

    Run generate .env file in postinstall script

--- scripts/gen-empty-env.ts
+++ scripts/gen-empty-env.ts
@@ -1,6 +1,11 @@
 import * as fs from 'fs-extra';
+import * as path from 'path';
 import BotServer from '../lib/bot-server';
 
-if (!fs.existsSync('.env')) {
-  BotServer.generateEnvFile('.env');
+const envPath = path.resolve(__dirname, '../.env');
+
+if (!fs.existsSync(envPath)) {
+  BotServer.generateEnvFile(envPath);
+  console.log('Generated environment file: ' + envPath);
+  console.log('Please configure environment variables before start the server.');
 }
